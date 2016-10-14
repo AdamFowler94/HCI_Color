4c54822c82af5a021a601c16bdae5b7cb229ad9c
adam fowler <115805f@acaidiau.ca>
2016-10-14 11:50:24 -0300

    Initial commit

---
+++ app/src/main/java/android/hci_group/com/hci_color/Main.java
@@ -0,0 +1,13 @@
+package android.hci_group.com.hci_color;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
